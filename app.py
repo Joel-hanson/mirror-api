85b1e13063e79b98b0a28323c8d8d22d0a5b8279
Joel-hanson <joelhanson025@gmail.com>
2019-02-13 10:51:56 +0530

    json dumps addedd

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask import jsonify
 import flask
+import json
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def hello():
     if flask.request.method == 'POST':
         if flask.request.args.get("request_for") == "hook":
-            return jsonify(flask.request.args.to_dict())
+            return jsonify(json.dumps(flask.request.args.to_dict()))
         return jsonify(flask.request.get_json())
     else:
         return jsonify(flask.request.args.to_dict())
